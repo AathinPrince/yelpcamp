b52ab3e5c745d339b104459e8e97da68e1fe462f
AathinPrince <aathinprince1@gmail.com>
2025-11-11 12:41:20 +0530

    feat: set up mongoose connection and define Campground model

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Database connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
 
-app.get('/',(req, res) => {
-    res.render('home')
+app.get('/makecampground',async (req, res) => {
+    const camp = new Campground({ title: 'my backyard' });
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, () => {
